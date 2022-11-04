718374c6e2fda9b7f6340368ecc33ff3e5befbd2
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-11-04 10:24:17 +0700

    delete data

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -14,9 +14,11 @@
       <h3>Daftar Mahasiswa</h3>
       <ul class="list-group">
         <?php foreach ($data['mhs'] as $mhs) : ?>
-          <li class="list-group-item d-flex justify-content-between align-items-center">
+          <li class="list-group-item">
             <?= $mhs['nama']; ?>
-            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary ">Detail</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge text-bg-danger float-end ml-1" onclick="return confirm('Yakin?')">Hapus</a>
+            <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge text-bg-primary float-end ml-1">Detail</a>
+
           </li>
         <?php endforeach; ?>
       </ul>
