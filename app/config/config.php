4c8b90628698774a39edb13bb38b0243798de651
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-11-04 09:21:07 +0700

    insert data

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
 define('BASEURL', 'http://localhost/mvc/public');
 
 // DB
-define('DB_HOST', '127.0.0.1:3310');
+define('DB_HOST', '127.0.0.1:3307');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'phpmvc');
--- app/core/Database.php
+++ app/core/Database.php
@@ -74,4 +74,9 @@ class Database
     $this->execute();
     return $this->stmt->fetch(PDO::FETCH_ASSOC);
   }
+
+  public function rowCount()
+  {
+    return $this->stmt->rowCount();
+  }
 }
